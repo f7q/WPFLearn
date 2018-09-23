7fbcfa9bf7873e613040af6f18b36b089b555479
f7Q <kokudou330@gmail.com>
2018-09-23 22:47:31 +0900

    add ViewModel

---
+++ Clickonce/WpfApp1/MainViiewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Application;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class MainViiewModel
+    {
+        public string Version
+        {
+            get
+            {
+                if (!ApplicationDeployment.IsNetworkDeployed) return string.Empty;
+
+                var version = ApplicationDeployment.CurrentDeployment.CurrentVersion;
+                return (
+                version.Major.ToString() + "." +
+                version.Minor.ToString() + "." +
+                version.Build.ToString() + "." +
+                version.Revision.ToString()
+　　　　　　    );
+            }
+        }
+    }
+}
